ae6659038b674e3740f378189f2a94bc5addec17
Bono Fox <git@bono-fox.de>
2025-09-23 22:59:59 +0200

    ✨ Pluginliste wird nach Namen sortiert

--- scripts/get-plugins.ts
+++ scripts/get-plugins.ts
@@ -4,7 +4,19 @@ import { KeepassPlugin } from "./keepass-plugin.interface";
 export const getPlugins = (): ReadonlyArray<KeepassPlugin> => {
   const pluginData = fs.readdirSync("plugin-data");
 
-  return pluginData.map((singlePlugin: string) =>
-    JSON.parse(fs.readFileSync(`plugin-data/${singlePlugin}`, "utf-8")),
-  );
+  return pluginData
+    .map((singlePlugin: string) =>
+      JSON.parse(fs.readFileSync(`plugin-data/${singlePlugin}`, "utf-8")),
+    )
+    .sort((a: KeepassPlugin, b: KeepassPlugin) => {
+      if (a.name < b.name) {
+        return -1;
+      }
+
+      if (a.name > b.name) {
+        return 1;
+      }
+
+      return 0;
+    });
 };
